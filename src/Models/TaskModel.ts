2b1a3f702a01feb5a1aaae99c284b4ebcff06b0d
Riyad139 <riyad.ahmed1886@gmail.com>
2023-01-21 02:26:18 +0600

    update task model

--- src/Models/TaskModel.ts
+++ src/Models/TaskModel.ts
@@ -1,42 +1,46 @@
 import mongoose from "mongoose";
 
-const TaskModel = new mongoose.Schema({
-  name: {
-    type: String,
-    require: [true, "A task must have a name"],
-    trim: true,
-  },
-  board: {
-    type: String,
-    enum: ["working", "todo", "completed"],
-  },
-  description: {
-    type: String,
-    require: [true, "A task need a description"],
-    trim: true,
-  },
-  label: {
-    type: String,
-  },
-  isDone: Boolean,
-  workedTime: [
-    {
-      startTime: Date,
-      endTime: Date,
+const TaskModel = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "A task must have a name"],
+      trim: true,
     },
-  ],
-  attachments: [
-    {
-      originalFileName: String,
-      fileId: String,
+    board: {
+      type: String,
+      enum: ["working", "todo", "completed"],
     },
-  ],
-  assingTo: [String],
-  createdAt: () => new Date(),
-  updatedAt: () => new Date(),
-  createdBy: String,
-  deadLine: Date,
-});
+    description: {
+      type: String,
+      require: [true, "A task need a description"],
+      trim: true,
+    },
+    label: {
+      type: String,
+    },
+    isDone: Boolean,
+    workedTime: [
+      {
+        startTime: Date,
+        endTime: Date,
+      },
+    ],
+    attachments: [
+      {
+        originalFileName: String,
+        fileId: String,
+      },
+    ],
+    assingTo: [String],
+
+    createdBy: String,
+    deadLine: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Task = mongoose.model("tasks", TaskModel);
 
