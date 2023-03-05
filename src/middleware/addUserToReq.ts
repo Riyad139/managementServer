7d749b4934903502e02894670d6aeadb080c7e86
Riyad139 <riyad.ahmed1886@gmail.com>
2023-03-06 03:35:47 +0600

    middle ware updated

--- src/middleware/addUserToReq.ts
+++ src/middleware/addUserToReq.ts
@@ -3,6 +3,7 @@ import * as Jwt from "jsonwebtoken";
 import { unless } from "express-unless";
 const addUserToReq: Controller = async (req, res, next) => {
   try {
+    if (req.path == "/api/user/signin" && req.method == "POST") return next();
     const salt = "YoNIggaThisIsASalt";
     if (!req.headers.cookie) return res.status(404).send("User not found");
     const id = Jwt.verify(req.headers.cookie.split("=")[1], salt);
