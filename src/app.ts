472669e934feee67119c9964e2c7f28ca021d36e
Riyad139 <riyad.ahmed1886@gmail.com>
2023-03-05 22:48:33 +0600

    now add mongo

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,21 @@
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
+import router from "./Route/route";
+import mongoose from "mongoose";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-//app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 //@ts-ignore
-// app.use(addUserToReq.unless({ path: ["/user/signup", "/user/signin"] }));
-// app.use(router);
+app.use(addUserToReq.unless({ path: ["/user/signup", "/user/signin"] }));
+app.use(router);
 //@ts-ignore
-//mongoose.connect(process.env.DB_URL).then((ms: any) => console.log("success"));
-
-app.get("/api/test", (req, res, next) => {
-  res.send("Hello");
-});
+mongoose.connect(process.env.DB_URL).then((ms: any) => console.log("success"));
 
 export default app;
