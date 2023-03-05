f2ffbd3bb312447cc00e6aba797093d479fe09ab
Riyad139 <riyad.ahmed1886@gmail.com>
2023-03-05 23:21:16 +0600

    add mongo

--- src/Route/test.ts
+++ src/Route/test.ts
@@ -1,9 +1,10 @@
 import Express from "express";
-
+import user from "../Models/users";
 const router = Express.Router();
 
-router.route("/user").get((req, res, next) => {
-  res.send("hello");
+router.route("/user").get(async (req, res, next) => {
+  const data = await user.find({});
+  res.send(data);
 });
 
 export default router;
